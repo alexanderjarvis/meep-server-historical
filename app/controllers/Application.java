60e109a030b154ea51e912a5c420c25c4a0884da
Alex Jarvis <alex@panaxiom.co.uk>
2010-11-15 18:25:02 +0000

    Modified the application response so that cookies are never sent to the client (to save bandwidth).

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,23 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import play.mvc.Finally;
+import play.mvc.Http;
 import controllers.oauth2.AccessTokenFilter;
 
 public class Application extends AccessTokenFilter {
+	
+	private static final Map<String, Http.Cookie> cookies = new HashMap<String, Http.Cookie>(0);
 
     public static void index() {
         render();
+    }
+    
+    @Finally
+    public static void removeCookies() {
+    	response.cookies = cookies;
     }
 
 }
