10759f7ab30a7c2532d1a4dd982442dbdcaf3e5a
Alex Jarvis <alex@panaxiom.co.uk>
2011-02-13 22:14:41 +0000

    The AccessTokenGenerator now checks if the generated token exists for a user and generates tokens until a unique one is created.

--- app/oauth2/AccessTokenGenerator.java
+++ app/oauth2/AccessTokenGenerator.java
@@ -1,13 +1,27 @@
 package oauth2;
 
+import models.User;
+
 import org.apache.commons.lang.RandomStringUtils;
 
+/**
+ * 
+ * @author Alex Jarvis axj7@aber.ac.uk
+ */
 public class AccessTokenGenerator {
 	
 	private static final int LENGTH = 32;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static String generate() {
-		return RandomStringUtils.randomAlphanumeric(LENGTH);
+		String accessToken = RandomStringUtils.randomAlphanumeric(LENGTH);
+		while (User.find("byAccessToken", accessToken) == null) {
+			accessToken = RandomStringUtils.randomAlphanumeric(LENGTH);
+		}
+		return accessToken;
 	}
 
 }
